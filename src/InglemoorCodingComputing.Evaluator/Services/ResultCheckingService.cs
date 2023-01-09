721c15c6cbe4a7295f55cb8651ff4931c325fd66
Albert Du <52804499+albert-du@users.noreply.github.com>
2023-01-08 16:14:00 -0800

    Update ResultCheckingService.cs

--- src/InglemoorCodingComputing.Evaluator/Services/ResultCheckingService.cs
+++ src/InglemoorCodingComputing.Evaluator/Services/ResultCheckingService.cs
@@ -5,23 +5,8 @@ public class ResultCheckingService : IResultCheckingService
 {
     private static string Clean(string x)
     {
-        var lines = x.Replace("\r\n", "\n").Replace('\r', '\n').Split('\n');
-        if (lines.Length is 0)
-            return string.Empty;
-
-        var start = 0;
-        for (; start < lines.Length; start++)
-        {
-            if (!string.IsNullOrEmpty(lines[start]))
-                break;
-        }
-        var end = lines.Length - 1;
-        for (; end > -1; end--)
-        {
-            if (!string.IsNullOrEmpty(lines[end]))
-                break;
-        }
-        return string.Join('\n', lines[start..(end + 1)]);
+        var lines = x.Replace("\r\n", "\n").Replace('\r', '\n').Split('\n').Where(x => !string.IsNullOrEmpty(x));
+        return string.Join('\n', lines);
     }
 
 
